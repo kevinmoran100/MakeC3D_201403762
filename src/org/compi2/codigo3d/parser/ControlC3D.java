82b92100c2b874c30dc48773de493f7394c5bc5a
kevinmoran100 <kevinalbertoorellana@gmail.com>
2017-03-17 20:26:25 -0600

    HOJA DE TRABAJO 3 - CORTOCIRCUITO LISTO Nombre:Kevin Alberto Morán Orellana Carnet: 201403762

--- src/org/compi2/codigo3d/parser/ControlC3D.java
+++ src/org/compi2/codigo3d/parser/ControlC3D.java
@@ -8,6 +8,7 @@ public class ControlC3D {
     
     private static int temp = 0;
     private static String c3d = "";
+    private static int templbl=0;
     
     /**
      * Reinicia las variables estáticas relacionadas con la generación del
@@ -16,6 +17,7 @@ public class ControlC3D {
     public static void reiniciar(){
         temp = 0;
         c3d = "";
+        templbl=0;
     }
     
     /**
@@ -24,6 +26,10 @@ public class ControlC3D {
      */
     public static String generaTemp(){
         return "t$"+temp++;
+    }
+    
+    public static String generaLabel(){
+        return "L"+templbl++;
     }
     
     /**
--- src/org/compi2/codigo3d/parser/NodoC3D.java
+++ src/org/compi2/codigo3d/parser/NodoC3D.java
@@ -8,13 +8,38 @@ package org.compi2.codigo3d.parser;
  */
 public class NodoC3D {
     private String cad;
+    private String v;
+    private String f;
+    
 
     public NodoC3D(String cad) {
         this.cad = cad;
+        this.v=null;
+        this.f=null;
+    }
+    
+    public NodoC3D(String cad, String v, String f) {
+        this.cad = cad;//verdadero
+        this.v=v;
+        this.f=f;
     }
     
     public String getCad(){
         return cad;
     }
+    
+    public String getV(){
+        return v;
+    }
+    
+    public String getF(){
+        return f;
+    }
+    
+    public boolean es_rel(){
+        return (this.f!=null) && (this.v!=null);
+    }
+    
+   
         
 }
